1a982a41db20de93f65dabd03612afb39f717df5
Bruno Emori <bruno.emori@gmail.com>
2017-05-05 09:19:32 -0300

    Parallel Matrix Multiplication

--- ProgramaçãoConcorrente/mm.c
+++ ProgramaçãoConcorrente/mm.c
@@ -7,12 +7,21 @@ typedef struct {
   int **matrix1, **matrix2;
   int start;
   int matrixHeight1, matrixWidth1, matrixWidth2; 
-  int *result;
+  int **result;
 } matrixArgs;
 
 void printMatrix(int **matrix, int height, int width) {
 	int i, j;
 	for (i = 0; i < height; i++) {
+		for (j = 0; j < width; j++) 
+			printf("%i ", matrix[i][j]);
+		printf("\n");
+	}
+}
+
+void dbgPrintMatrix(int **matrix, int height, int width, int start) {
+	int i, j;
+	for (i = start; i < start + height; i++) {
 		for (j = 0; j < width; j++) 
 			printf("%i ", matrix[i][j]);
 		printf("\n");
@@ -60,52 +69,70 @@ int **multiplyMatrix(int **matrix1, int **matrix2, int height1, int width1, int 
 //Parallel
 void *parMulti(void *args) {
   matrixArgs *mArgs = args;
-  mArgs->result = createMatrix(mArgs->height, mArgs->width);
+  mArgs->result = createMatrix(mArgs->matrixHeight1, mArgs->matrixWidth2);
   int i, j, k;
-  int i = mArgs->line;
 
-  for (i = mArgs->start; i < mArgs->height1; i++) {
-    for (j = 0; j < mArgs->width2; j++)  
-      int aux = 0
-        for (k = 0; k < width1; k++)
-        aux += mArgs->matrix1[mArgs->line][k] * mArgs->matrix2[k][j];
-
-    mArgs->result[i][j] = aux;
+  for (i = mArgs->start; i < mArgs->start + mArgs->matrixHeight1; i++) {
+    for (j = 0; j < mArgs->matrixWidth2; j++) {
+    int aux = 0;
+        for (k = 0; k < mArgs->matrixWidth1; k++)
+          aux += mArgs->matrix1[i][k] * mArgs->matrix2[k][j];
+        mArgs->result[i - mArgs->start][j] = aux;
+    }
   }
 }
 
 int **parMultiplyMatrix(int **matrix1, int **matrix2, int height1, int width1, int width2, int numThreads) {
-	int **resultMatrix = createMatrix(matrixHeight1, matrixWidth2);
-	int i = 0, k, currentLine = 0;
+	int **resultMatrix = createMatrix(height1, width2);
+	int i = 0, k;
 	
   pthread_t *thread = malloc(sizeof(pthread_t *) * numThreads);
-
   matrixArgs *mArgs = malloc(sizeof(matrixArgs) * numThreads);
 
   int linesPerThread = height1 / numThreads;
-  int rest = height % numThreads;
+  int rest = height1 % numThreads;
 
-  mArgs[0].matrix1 = matrix1;
-  mArgs[0].matrix2 = matrix2;
+  for (k = 0; k < numThreads; k++) {
+    mArgs[k].matrix1 = matrix1;
+    mArgs[k].matrix2 = matrix2;
+    mArgs[k].matrixWidth1 = width1;
+    mArgs[k].matrixWidth2 = width2;
+    mArgs[k].matrixHeight1 = 0;
+  }
+
+  k = 0;
+  while (rest > 0) {
+    if (k == numThreads) 
+      k = 0;
+
+    mArgs[k].matrixHeight1++;
+    rest--;
+    k++;
+  }
+
   mArgs[0].start = 0;
-  mArgs[0].matrixHeight1 = linesPerThread;
-  mArgs[0].matrixWidth1 = width1;
-  mArgs[0].matrixWidth2 = width2;
+  mArgs[0].matrixHeight1 += linesPerThread;
 
   for (k = 1; k < numThreads; k++) {
-    mArgs[k].start = mArgs[k - 1].start + linesPerThread - 1;
+    mArgs[k].start = mArgs[k - 1].start + mArgs[k - 1].matrixHeight1;
+    mArgs[k].matrixHeight1 += linesPerThread;
   }
 
-  for (i = 0; i < height1; i++) {
-  }
-
-    for (k = i; k < threadsCreated; k++) {
-      resultMatrix[i] = mArgs[k].result;
+  for (k = 0; k < numThreads; k++) {
+    if (pthread_create(&thread[k], NULL, parMulti, &mArgs[k])) {
+      printf("Error creating thread %i\n", k);
+	    free(thread);
+      return NULL;
     }
   }
+
+  for (k = 0; k < numThreads; k++) {
+    pthread_join(thread[k], NULL);
+    printMatrix(mArgs[k].result, mArgs[k].matrixHeight1, mArgs[k].matrixWidth2);
+  }
+
 	free(thread);
-  free(matrixArgs)
-	return resultMatrix;
+  return NULL;
 }
 
 
@@ -129,7 +156,7 @@ int main(int argc, char** argv) {
   */
 
   //resultMatrix = multiplyMatrix(matrix1, matrix2, matrixHeight1, matrixWidth1, matrixWidth2);
-  //resultMatrix = parMultiplyMatrix(matrix1, matrix2, matrixHeight1, matrixWidth1, matrixWidth2, nThreads);
+  resultMatrix = parMultiplyMatrix(matrix1, matrix2, matrixHeight1, matrixWidth1, matrixWidth2, nThreads);
 
 	//printMatrix(resultMatrix, matrixWidth1, matrixHeight2);
 }
