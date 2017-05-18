e1d54a99a9967618f7a1111fe11f9aa2b9b4e7b8
Bruno Emori <bruno.emori@gmail.com>
2017-05-18 14:46:40 -0300

    Changed optimization from command line

--- ProgramaçãoConcorrente/polybench-c-4.2.1-beta/run.bash
+++ ProgramaçãoConcorrente/polybench-c-4.2.1-beta/run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc -O3 -I utilities/ -I stencils/jacobi-2d/ utilities/polybench.c stencils/jacobi-2d/jacobi-2d.c -DPOLYBENCH_TIME -o jacobi2d-sequential
-gcc -pthread -O3 -I utilities/ -I stencils/jacobi-2d/ utilities/polybench.c stencils/jacobi-2d/jacobi-2d-pthreads.c -DPOLYBENCH_TIME -o jacobi2d-pthread
-gcc -fopenmp -O3 -I utilities/ -I stencils/jacobi-2d/ utilities/polybench.c stencils/jacobi-2d/jacobi-2d-openmp.c -DPOLYBENCH_TIME -o jacobi2d-openmp
+gcc -I utilities/ -I stencils/jacobi-2d/ utilities/polybench.c stencils/jacobi-2d/jacobi-2d.c -DPOLYBENCH_TIME -o jacobi2d-sequential
+gcc -pthread -I utilities/ -I stencils/jacobi-2d/ utilities/polybench.c stencils/jacobi-2d/jacobi-2d-pthreads.c -DPOLYBENCH_TIME -o jacobi2d-pthread
+gcc -fopenmp -I utilities/ -I stencils/jacobi-2d/ utilities/polybench.c stencils/jacobi-2d/jacobi-2d-openmp.c -DPOLYBENCH_TIME -o jacobi2d-openmp
 echo
